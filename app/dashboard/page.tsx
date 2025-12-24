a1a5e2940ad641e21d18b6259361b0b671d5194d
Neha <naiharana173@gmail.com>
2025-12-24 19:01:57 +0500

    initial commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+export default function DashboardPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-semibold">
+        Dashboard
+      </h1>
+    </div>
+  )
+}
